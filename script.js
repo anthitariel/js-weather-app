0e2c008ffadbf6ca7dd2d26d719d7a6565c6e36f
Anfisa Domashova <anfisa.domashova@accesa.eu>
2023-11-29 19:59:18 +0200

    changed font size & padding, added margin

--- script.js
+++ script.js
@@ -31,14 +31,13 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     return `
       <div class="current-weather">
         <div class="details">
-          <h2>${cityName}, ${dayOfWeek} ${weatherItem.dt_txt.split(" ")[0]}</h2>
+          <h2>${cityName}, ${weatherItem.dt_txt.split(" ")[0]}</h2>
           <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
           <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
           <h6>Humidity: ${weatherItem.main.humidity}%</h6>
         </div>
         <div class="icon">
           <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-          <h6>${weatherItem.weather[0].description}</h6>
         </div>
       </div>
     `;
